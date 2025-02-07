48e5fd2fcb31c2e5a1d56697f263e07e51da5af6
schroderian <ian.schroder@testpro.io>
2025-02-07 17:40:10 -0600

    homework16

---
+++ src/test/java/Homework16.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework16 {
+    @Test
+    public void registrationNavigation(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+
+        WebElement registrationLink = driver.findElement(By.cssSelector("a[href='registration']"));
+        registrationLink.click();
+
+        Assert.assertEquals(driver.getCurrentUrl(),"https://qa.koel.app/registration");
+
+        driver.quit();
+
+    }
+}
+
